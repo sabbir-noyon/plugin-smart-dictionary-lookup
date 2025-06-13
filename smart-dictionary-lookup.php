a0c71bd736c4cc33a19c456f76c777cc69887e1f
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2025-06-13 16:21:47 +0600

    Codes pushed as per WP Coding Standard

--- smart-dictionary-lookup.php
+++ smart-dictionary-lookup.php
@@ -1,59 +1,35 @@
 <?php
-
 /**
  * Plugin Name: Smart Dictionary Lookup
  * Description: A plugin that shows word definitions when double-clicked in post content using a public dictionary API.
- * Plugin URI: https://wordpress.org/plugins/smart-dictionary-lookup
- * Version: 1.0.0
- * Author: Sabbir Noyon
- * Author URI: https://www.sabbir-noyon.com
+ * Plugin URI:  https://wordpress.org/plugins/smart-dictionary-lookup
+ * Version:     1.0.0
+ * Author:      Sabbir Noyon
+ * Author URI:  https://www.sabbir-noyon.com
  * Text Domain: smart-dictionary-lookup
  * Domain Path: /languages
  */
 
- if (!defined('ABSPATH')){ 
-   exit;
- }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-
-require_once plugin_dir_path(__FILE__) . 'includes/class-sdl-core.php';
-require_once plugin_dir_path(__FILE__) . 'includes/class-sdl-admin.php';
-
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-sdl-core.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-sdl-admin.php';
 
 final class Smart_Dictionary_Lookup {
 
-  public function __construct() {
+	public function __construct() {
+		add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
+	}
 
-    add_action('plugins_loaded', [$this,'init_plugin' ]);
+	public function init_plugin() {
+		if ( is_admin() ) {
+			new SDL_Admin();
+		}
 
-
-  }
-
-
-  public function init_plugin(){
-
-    if( is_admin() ){
-
-      new SDL_Admin();
-
-    }
-
-    new SDL_Core();
-
-
-  }
-
-
-
-
-
-
-
-
-
-
+		new SDL_Core();
+	}
 }
 
-
-
- new Smart_Dictionary_Lookup();
+new Smart_Dictionary_Lookup();
